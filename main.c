1bcc92eb60da3ca8ab2839cedec22bfbfceb9211
YongJun <dkfdirvkdnj@gmail.com>
2023-08-28 17:59:19 +0900

    0828-git/1 스켈레톤 작성

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int add(int, int);
+int sub(int, int);
+int div(int, int);
+int multi(int, int);
+
+
+
+int main (int argc, char* arv[]) {
+
+    int a, b, c;
+    char operator;
+
+    printf("Please enter a expression");
+    scanf("%d %c %d", &a, &operator, &b);
+
+    switch (operator) {
+        case '+':
+            c = add(a, b);
+            break;
+
+        case '-':
+            c = sub(a, b);
+            break;
+
+        case '*':
+            c = multi(a, b);
+            break;
+            
+        case '/':
+            c = div(a, b);
+            break;
+    }
+
+
+    return 0;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
